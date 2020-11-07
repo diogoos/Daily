ea3e32271845eb86d442ceab9bbe2bcbf2a7bae2
daemonleaf <59505963+daemonleaf@users.noreply.github.com>
2020-11-07 08:16:05 -0300

    Initial Commit

---
+++ Daily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily
+//
+//  Created by Diogo Silva on 11/07/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
